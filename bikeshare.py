cd2bcef2d3653d71ecf1957a4363ecbaf2253d5b
Mahmood Makhloof <73743979+MahmoodMakhloof@users.noreply.github.com>
2021-02-25 01:59:16 +0200

    update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -27,7 +27,7 @@ def get_filters():
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     Cities =['chicago','new york city','washington']
     while True:
-        city = (input('Which City would you need to analyze (Chicago , New york city , Washington) ?\n')).lower()
+        city = (input('Which City would you need to analyze (Chicago , New York City , Washington) ?\n')).lower()
         if city in Cities:
             break
     print( "ANALYZING "+city.upper() +" ......................")
